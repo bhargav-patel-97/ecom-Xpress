567b2c626a9010955c87282a25cddc92cbb1d542
bhargav <bhargavpatel6040@gmail.com>
2020-05-06 22:32:18 +0530

    Subject: Adding Router in the webapp Message: Setting up router and implementing in the homepage.

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
+
 const HomePage = () => (
     <div className='homepage'>    
         <Directory />
     </div>
 );
 
-export default HomePage;
+export default (HomePage);
