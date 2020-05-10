422157008934c86a48766dbf36793ee0ec642323
bhargav <bhargavpatel6040@gmail.com>
2020-05-10 21:48:23 +0530

    Updates

---
+++ precache-manifest.808883fdace528e3ac78326fe76db126.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cacda1df0d04ce7a3d17fca46607290",
+    "url": "/ecom-Xpress/index.html"
+  },
+  {
+    "revision": "d5504ad6b5004e60f8f6",
+    "url": "/ecom-Xpress/static/css/main.ea8fb4f8.chunk.css"
+  },
+  {
+    "revision": "73fc9fb79643f39473d5",
+    "url": "/ecom-Xpress/static/js/2.a86c1a99.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/ecom-Xpress/static/js/2.a86c1a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5504ad6b5004e60f8f6",
+    "url": "/ecom-Xpress/static/js/main.b01e7013.chunk.js"
+  },
+  {
+    "revision": "79ee05b678b3ab609f8c",
+    "url": "/ecom-Xpress/static/js/runtime-main.a3ca5739.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/ecom-Xpress/static/media/4.4 crown.svg.19ef7b17.svg"
+  }
+]);
