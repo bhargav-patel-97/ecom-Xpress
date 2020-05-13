46e992aa731aed41efefcb3af579f0d21ccb6305
bhargav <bhargavpatel6040@gmail.com>
2020-05-13 22:46:29 +0530

    Subject: Small style and functional quick fixes.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -56,9 +56,9 @@ class SignIn extends React.Component {
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
                     </div>
                 </form>
+                <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
             </div>
         )
     }
