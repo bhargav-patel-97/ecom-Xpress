61ec8e6934bbf2c7d3b33ad32162322a9a6f08d9
bhargav <bhargavpatel6040@gmail.com>
2020-05-16 19:04:15 +0530

    Subject: Cart & Redux Message: Implementing cart features and functionalities using Redux. Setting up selectors for Redux.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems } from '../../redux/cart/cart.selector';
+import { connect } from 'react-redux';
+
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+            {
+                cartItems.length ? (
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                ) : (
+                    <span className='empty-message'>Your cart is empty.</span>
+                )
+            }
+        </div>
         <CustomButton>CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = createStructuredSelector ({
+    cartItems: selectCartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
